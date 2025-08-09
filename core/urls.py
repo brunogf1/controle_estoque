3eff71e07a774a09b01508fae23ff4bb04aec272
bruno <bruno.golinf@gmail.com>
2025-08-09 17:47:36 -0300

    Login, Menu, Listagem de Produtos e Criacao de OS. Estilizacao de todos

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('menu/', views.menu_view, name='menu'),
-    
-  #  path('itens/', views.visualizar_itens, name='itens'),
+    path('itens/', views.visualizar_itens, name='visualizar_itens'),
+    path('ordemServico', views.criar_ordem_servico, name='criar_ordem_servico'),
   #  path('movimentacao/', views.movimentacao_estoque, name='movimentacao_estoque'),
   #  path('logout/', views.logout, name='logout'),
 ]
